60e7593a0eecb49307314a41960f31aa7108525e
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2023-06-03 23:01:14 +0530

    Create crawlWeb.js

---
+++ crawlWeb.js
@@ -0,0 +1,57 @@
+import cheerio from "cheerio";
+import axios from "axios";
+
+export default async function crawlWeb(url) {
+  // working urls
+  // https://www.spotify.com/in-en/legal/end-user-agreement/
+  // https://www.ubuy.co.in/privacy-policy
+
+  const extractedText = [];
+
+  return await axios
+    .get(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $("p").each((index, element) => {
+        const text = $(element).text().trim();
+        extractedText.push(text);
+      });
+
+      extractedText.forEach((text) => {
+        console.log(text);
+      });
+
+      return { extractedText, status: "ok" };
+      // Further code for extracting text goes here
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+      return {
+        error,
+        status: "error",
+      };
+    });
+}
+
+// crawlWeb("https://www.ubuy.co.in/privacy-policy").then((res) =>
+//   console.log(res)
+// );
+
+// const urls = [
+//   "https://www.spotify.com/in-en/legal/end-user-agreement",
+//   "https://www.ubuy.co.in/privacy-policy",
+// ];
+
+// let result = urls;
+// urls.map(async (url, index) => {
+//   crawlWeb(url)
+//     .then((res) => {
+//       console.log(res);
+//       if (res.status === "ok") result[index] = res.extractedText;
+//     })
+//     .catch((error) => console.log("error: ", error));
+// });
+
+// console.log(result);
