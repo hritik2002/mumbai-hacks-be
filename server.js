c2deab2e3950a4ce777613ede21341f1e0058c7f
akshaykapadi <akshaykapadi8@gmail.com>
2023-06-04 03:36:36 +0530

    apis

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import bodyParser from "body-parser";
 import crawlWeb from "./crawlWeb.js";
 import multer from "multer";
 import fs from "fs";
+import pdfjs from "pdfjs-dist"
+import mammoth from "mammoth"
 const upload = multer({ dest: "uploads/" });
 import path from 'path'
 
@@ -16,23 +18,248 @@ app.get("/", (req, res) => {
   res.json({ data: "Healthy" });
 });
 
-app.post("/upload", upload.single("path"), (req, res) => {
-  console.log("req.file:", req.file);
+const promptArr = [
+  "In the above agreement give me the result of parties involved in the following json format for party type and party name, the result should be JSON only",
+  "Give me the number of risks, the gravity of risks, number of advantages and gravity of advantages for in the following format json for [__ELEMENTS__]  each in the above legal document in the following format json for"
+];
+
+const stringifyJsonprompt = JSON.stringify({"parties_in_doc":{"party1":{"name":"{{party1name}}","type":"{{party1type}}"},"party2":{"name":"{{party2name}}","type":"{{party2type}}"},"partyN":{"name":"{{partyNname}}","type":"{{partyNtype}}"}}});
+promptArr[0] += stringifyJsonprompt;
+
+const promptJson = {
+ 
+		"numberOfRisks": "X no of risks of __ELEMENT__ ",
+		"gravityOfRisks": "high | medium | low",
+		"risks": {
+			"riskX": "details of risk of __ELEMENT__ in the string",
+		},
+		"numberOfAdvantages": "Y no of advantage __ELEMENT__",
+		"gravityOfAdvantages": "high | medium | low",
+		"Advantages": {
+			"AdvantageY": "details of Advantage of __ELEMENT__ in the string"
+		}
+
+}
+
+app.post("/upload", upload.single("file"), async(req, res) => {
   try {
-    const destinationPath = `./file.pdf`;
-    fs.renameSync(req.file.path, destinationPath);
+    const bodyContent = req.body.content;
+   
+    if (!req.file && !bodyContent) {
+      return res.status(400).send('No file uploaded.');
+    }
 
-    const fileContent = fs.readFileSync(destinationPath, "utf-8");
-    console.log("fileContent:", fileContent);
+    let filePath;
+    let fileExtension;
 
-    fs.unlinkSync(destinationPath);
+    if(req.file){
+       filePath = req.file.path;
+       fileExtension = path.extname(req.file.originalname).toLowerCase();
+    }
+    console.log("fileExtension", fileExtension);
+    let fileContent = "";
+    if(fileExtension === ".pdf"){
+      fileContent = await readPDF(filePath);
+    console.log("fileContent pdf", fileContent);
 
-    res.send(fileContent);
+    }else if(fileExtension === ".docx" || fileExtension === ".doc"){
+      fileContent = await readDocFile(filePath);
+      console.log("docx file content",fileContent);
+      
+    }else if(typeof bodyContent === "string"){
+      fileContent = bodyContent;
+    }
+   
+    const data = await getAIData(fileContent)
+    // const data = "asd"
+
+    const uniqueKey = generateUniqueKey();
+
+    if(data && uniqueKey && fileContent){
+      getAIData(fileContent, promptArr[0], uniqueKey, 1);
+    }
+
+    // if(filePath){
+    //   await fs.unlink(filePath);
+    // }
+    
+    res.send({"data": data, "uniqueKey" : uniqueKey, idx:0});
+
+
   } catch (err) {
-    console.error(err);
+    console.error("error printing....",err);
     res.status(500).send("Error reading or saving the file.");
   }
 });
+
+app.post("/polling", async(req,res) => {
+
+  const { key, idx } = req.body;
+  const filename = `${key}--${idx}.txt`;
+
+  const filePath = "results/" + filename;
+  console.log(filePath);
+  // return
+  fs.readFile(filePath, 'utf8', (error, data) => {
+    if (error) {
+      console.error('Error reading file:', error);
+      return res.status(500).send('Error reading file.');
+    }
+
+    if(!data){
+      res.status(500).send("Oops! Something went wrong. Please check the file again.");
+    }
+
+    try{
+      const jsonData = JSON.parse(data);
+      res.send({data : jsonData ,  uniqueKey : key, idx});
+    }catch{
+      res.send({data : data ,  uniqueKey : key, idx});
+    }
+  });
+
+});
+
+const getAIData = async(content, prompt="", uniqueKey, idx) => {
+  console.log("prompt", prompt)
+    if(!prompt){
+      prompt = "/n/ntl;dr";
+    }
+    const promptClass = new PromptMeClass(content);
+    const data = await promptClass.PromptMe(prompt);
+    console.log("idx", idx);
+    console.log("uniqueKey", uniqueKey);
+
+    if(uniqueKey && idx){
+      if(idx === 1){
+        console.log("indx 1 data", data);
+        await saveFile(uniqueKey, idx, data);
+        const partyArr = [];
+        const partiesInDoc = typeof data === "string" ? JSON.parse(data) : data;
+
+        for (const key in partiesInDoc["parties_in_doc"]) {
+          if (partiesInDoc["parties_in_doc"].hasOwnProperty(key)) {
+            const type = partiesInDoc["parties_in_doc"][key].type;
+            
+            partyArr.push(type);
+          }
+        }
+
+        const uniqueValues = [...new Set(partyArr)];
+        console.log("uniqueValues", uniqueValues)
+
+        const arrayElement = formatArrayElements(uniqueValues);
+        let prompt = promptArr[1].replace("__ELEMENTS__", arrayElement);
+        let outputPromptJsonObj = getUpdatedPromptJson(uniqueValues);
+        prompt += "\n";
+        prompt += JSON.stringify(outputPromptJsonObj);
+        
+        await getAIData(content, prompt, uniqueKey, idx+1)
+      }else{
+        console.log("data 2", data);
+        await saveFile(uniqueKey, idx, data);
+      }
+    }
+
+    console.log("data123", data)
+    return data;
+}
+
+function formatArrayElements(array) {
+  const output = array.join(', ').replace(/,([^,]*)$/, ' and$1');
+  return output;
+}
+
+const getUpdatedPromptJson = (element) => {
+  const obj = {};
+  // console.log(element);
+  for(const e of element){
+      obj[e] = replaceElement(promptJson,e);
+      console.log("promptJson",obj[e]);
+  }
+  return obj;
+}
+
+function replaceElement(obj, replaceWith) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      replaceElement(obj[key], replaceWith);
+    } else if (typeof obj[key] === 'string' && obj[key].includes('__ELEMENT__')) {
+      obj[key] = obj[key].replace('__ELEMENT__', replaceWith);
+    }
+  }
+  console.log("obj data replace element", obj);
+  return obj;
+}
+
+
+
+const saveFile = (key, idx, data) => {
+  const fileName = `${key}--${idx}.txt`;
+  const filePath = "results/" + fileName
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, data, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        console.log("sucess")
+        resolve();
+      }
+    });
+  });
+}
+
+const readPDF = async(filename) => {
+  const data = new Uint8Array(fs.readFileSync(filename));
+  const doc = await pdfjs.getDocument(data).promise;
+  const numPages = doc.numPages;
+  
+  let text = '';
+  for (let i = 1; i <= numPages; i++) {
+    const page = await doc.getPage(i);
+    const content = await page.getTextContent();
+    const pageText = content.items.map(item => item.str).join(' ');
+    text += pageText + ' ';
+  }
+  
+  return text;
+}
+
+const readDocFile = async(filename) => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      convertImage: mammoth.images.imgElement(function (element) {
+        return element.read('base64');
+      }),
+    };
+
+    mammoth.extractRawText({ path: filename }, options)
+      .then(result => {
+        const content = result.value.trim(); // Trim extra whitespace
+        resolve(content);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+
+}
+
+const generateUniqueKey = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let key = '';
+  
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    key += characters[randomIndex];
+  }
+  
+  return key;
+}
+
+
+
 
 app.post("/analyze", (req, res) => {
   const { urls } = req.body;
